db61a1b74d131a16c955fbf74cadedeade932ac1
annyn <ennibondarenko@gmail.com>
2023-03-17 22:51:17 +0400

    done

---
+++ app.js
@@ -0,0 +1,15 @@
+const api_link = "https://api.adviceslip.com/advice";
+const button = document.querySelector(".button");
+const adviceNumber = document.querySelector(".advice-number");
+const adviceQuote = document.querySelector(".advice-quote");
+
+async function generate() {
+    const response = await fetch("https://api.adviceslip.com/advice");
+    const advice = await response.json();
+    console.log(advice);
+
+    document.querySelector(".advice-number").innerText = `ADVICE #${advice.slip.id}`;
+    document.querySelector(".advice-quote").innerText = `"${advice.slip.advice}"`
+}
+
+button.addEventListener('click', generate); 
